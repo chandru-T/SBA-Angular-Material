a9ab37c95e74b3485f80ac93f24fbc2b34f94c94
unknown <ctsjavauser116@iiht.tech>
2020-06-23 03:17:00 +0530

    Complted the Add/update Task Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'users', component: UserComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'tasks', component: TaskComponent },
+  { path: 'addTask', component: AddTaskComponent },
+  { path: 'EditTaskComponent', component: AddTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UserPipe } from './user/user.pipe';
+import { ProjectPipe } from './projects/project.pipe';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    EditTaskComponent,
+    UserComponent,
+    TaskComponent,
+    ProjectsComponent,
+    UserPipe,
+    ProjectPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: appearance}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
